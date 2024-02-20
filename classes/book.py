4fac016681c4d0076b82a20fdcc81ea20b42bbf9
paulaito <pakemi.ito@gmail.com>
2024-02-20 21:55:43 +0000

    Move book.py and library.py to "classes" folder

--- classes/book.py
+++ classes/book.py
@@ -2,6 +2,8 @@ class Book:
     def __init__(self, id: int, score: int):
         self.id = id
         self.score = score
+        self.library_id = None
+        self.scanned = False
 
     def __str__(self):
         return f"(id: {self.id}, score: {self.score})"
