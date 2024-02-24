ecef843999d95cfa39a3121ad54e0f0baa74702a
Ingrid Diniz <ingriddiniz@gmail.com>
2024-02-24 11:03:55 +0000

    Started Hill Climbing Solution

---
+++ metaheuristics/hill_climbing.py
@@ -0,0 +1,6 @@
+from metaheuristics.solver import Solver 
+
+class HillClimbingSolver(Solver):
+    def __init__(self, total_books, libraries, total_days):
+        super().__init__(total_books, libraries, total_days)
+    
