89f7d24ddf97459a9c5e35167d8face445486ef1
paulaito <pakemi.ito@gmail.com>
2024-02-26 12:28:44 +0000

    Implemented Genetic Algorithms

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 from helpers.file_reader import FileReader
-from metaheuristics.solver import Solver
+from metaheuristics.genetic_algorithm import GeneticAlgorithm
 
 def solve(file_name):
     file_reader = FileReader()
     total_books, libraries, total_days = file_reader.read(file_name)
     
-    parent_solver =  Solver(total_books, libraries, total_days)
-
-    print("Random Initial Solution")
-    random_initial_solution = parent_solver.create_initial_solution("random")
-    print(random_initial_solution)
-    parent_solver.clear()
-    new_neighbor = parent_solver.get_internal_neighbour(random_initial_solution,"remove")
-    print("Neighbor Solution")
-    print(new_neighbor)
-    
-    #print()
-    
-#
-    #print("Greedy Initial Solution")
-    #greedy_initial_solution = parent_solver.create_initial_solution("greedy")
-    #print(greedy_initial_solution)
+    ga =  GeneticAlgorithm(total_books, libraries, total_days)
+    ga.solve(
+        population_size=25,
+        n_generations=5,
+        mutate_mode="random",
+        crossover_mode="mid"
+    )
 
 if __name__ == "__main__":
     solve("data/a_example_2.in")
