c5230d73dab19de902c7a3ecee03e6f4bce857bb
paulaito <pakemi.ito@gmail.com>
2024-02-27 14:46:31 +0000

    (wip) GA results analysis

--- analysis/ga/write_csv.py
+++ analysis/ga/write_csv.py
@@ -2,4 +2,4 @@
 import pandas as pd
 
 ga_results = pd.DataFrame(columns=["filename", "population_size", "n_generations", "mutate_mode", "crossover_mode", "best_score", "fittest", "time"])
-# ga_results.to_csv("analysis/ga_results.csv", index=None)
+ga_results.to_csv("analysis/ga/ga_results.csv", index=None)
