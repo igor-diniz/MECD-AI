71f0a740c7ffbbe7d61602ef03e84553c9a5eb13
Paula Ito <paula.ito@farfetch.com>
2024-02-27 09:40:15 +0000

    Run main as grid search

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from helpers.file_reader import FileReader
 from metaheuristics.genetic_algorithm import GeneticAlgorithm
 
-def solve(file_name):
+def solve(file_name, population_size, n_generations, mutate_mode, crossover_mode):
     file_reader = FileReader()
     total_books, libraries, total_days = file_reader.read(file_name)
     
-    ga =  GeneticAlgorithm(total_books, libraries, total_days)
+    ga = GeneticAlgorithm(total_books, libraries, total_days)
     ga.solve(
-        population_size=50,
-        n_generations=10,
-        mutate_mode="random",
-        crossover_mode="mid",
+        population_size=population_size,
+        n_generations=n_generations,
+        mutate_mode=mutate_mode,
+        crossover_mode=crossover_mode,
         results_csv="analysis/ga/ga_results.csv",
         filename=file_name
     )
 
 if __name__ == "__main__":
-    solve("data/b_read_on.in")
+    # Define the grid of parameters to search
+    population_size = 25
+    n_generations = 10
+    mutate_modes = ["swap", "addition", "deletion"]
+    crossover_modes = ["mid", "random"]
+
+    # Perform grid search
+    for mutate_mode in mutate_modes:
+        for crossover_mode in crossover_modes:
+            solve("data/f_libraries_of_the_world.in", population_size, n_generations, mutate_mode, crossover_mode)
