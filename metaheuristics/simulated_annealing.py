fdd5b5568301b75c2cf9272c0756290657846d4b
Ingrid Diniz <ingriddiniz@gmail.com>
2024-02-28 22:19:57 +0000

    Fixing hc and sa solver functions

--- metaheuristics/simulated_annealing.py
+++ metaheuristics/simulated_annealing.py
@@ -2,6 +2,7 @@ from metaheuristics.solver import Solver
 from helpers.solution import Solution
 from copy import deepcopy
 import numpy as np
+from helpers.utils import results_to_csv
 import random
 import time
 import tracemalloc
@@ -9,40 +10,28 @@ import tracemalloc
 class SimulatedAnnealing(Solver):
     def __init__(self, total_books, libraries, total_days):
         super().__init__(total_books, libraries, total_days)
-        self.initial_solution = super().create_initial_solution(mode="random")
-        self.internal_neighbors_generator = super().get_internal_neighbour
-        self.external_neighbors_generator = super().get_external_neighbour
-
-    def select_initial_solution_type(self, initial_solution_random, initial_solution_greedy):
-        selected_initial_solution = random.choice(["random", "greedy"])
-        if selected_initial_solution == "random":
-            initial_solution = initial_solution_random
-        else: 
-            initial_solution = initial_solution_greedy
-        return initial_solution
+        self.curr_sol_history = []
 
     def evaluate_solution(self, solution):
         total_score = solution.evaluate()
         return total_score
    
-    def select_neighbor_generator(self, solution: Solution, internal_neighbors_generator, external_neighbors_generator):
-        choice = ["internal", "external"]
-        selected_generator = random.choice(choice)
+    def select_neighbor_generator(self, solution: Solution):
+        selected_generator = random.choice(["internal", "external"])
         if selected_generator == "external":
-            neighbor_generator = external_neighbors_generator(solution)
+            neighbor_generator = self.get_external_neighbour(solution)
         else: 
-            neighbor_generator = internal_neighbors_generator(solution, "swap")
+            neighbor_generator = self.get_internal_neighbour(solution, "swap")
         return  neighbor_generator   
    
     def solve(
-            self, initial_solution_random,
-            initial_solution_greedy,
-            internal_neighbors_generator,
-            external_neighbors_generator,
+            self, initial_solution,
             num_iterations: int,
             T: float, 
             cooling_schedule: float,
-            log=False
+            log=False,
+            results_csv = None,
+            filename = None
             ):
             
             start_time = time.time()
@@ -53,7 +42,7 @@ class SimulatedAnnealing(Solver):
 
             iteration = 0
             temperature = T
-            solution = self.select_initial_solution_type(initial_solution_random,initial_solution_greedy)  # Best solution after 'num_iterations'
+            solution = initial_solution
             score = self.evaluate_solution(solution)
 
             best_solution = deepcopy(solution)
@@ -62,7 +51,7 @@ class SimulatedAnnealing(Solver):
             while iteration < num_iterations:
                 temperature *= cooling_schedule
                 iteration += 1
-                neighbor_solution = self.select_neighbor_generator(solution, internal_neighbors_generator, external_neighbors_generator) 
+                neighbor_solution = self.select_neighbor_generator(best_solution)
                 neighbor_score = self.evaluate_solution(neighbor_solution)
 
                 delta = neighbor_score - score
@@ -77,10 +66,15 @@ class SimulatedAnnealing(Solver):
                         if log:
                             print("\nBest Neighbour Selected:\n", neighbor_solution)
                             print("\nBest Solution So Far:\n", best_solution)
+
             end_time = time.time()
             elapsed_time = end_time - start_time
             _, peak_memory = tracemalloc.get_traced_memory()
             tracemalloc.stop()
+
+            if results_csv and filename:
+                results_to_csv(results_csv, filename, best_solution, score, elapsed_time, peak_memory)
+                print(f"Result written to {results_csv}.")
 
             print(f"-----\nElapsed time: {elapsed_time} seconds\nPeak memory: {peak_memory} bytes")
             print("\nFinal Solution:\n", best_solution)
