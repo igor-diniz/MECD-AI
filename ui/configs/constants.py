ea8df26185837b30d490a26f756ead253782c532
paulaito <pakemi.ito@gmail.com>
2024-03-13 11:10:21 +0000

    Created constants

---
+++ ui/configs/constants.py
@@ -0,0 +1,61 @@
+FONT_TITLE = ("Arial", 18)
+FONT_SUBTITLE = ("Arial", 15)
+FONT_TEXT = ("Arial", 12)
+FONT_BUTTON_SMALL = ("Arial", 11)
+FONT_RUN = ("Arial", 14)
+FONT_AUTHORS_LABEL = ("Arial", 9)
+
+AUTHORS_LABEL_TEXT = "Developed by Ingrid Diniz, Igor Diniz and Paula Ito"
+
+MAIN_TITLE = "Book Scanning Optimization"
+HOMEPAGE_SUBTITLE = "Welcome to the Book Scanning Optimization UI."
+HOMEPAGE_TEXT = "What would you like to do?"
+
+BUTTON_HOMEPAGE_INDIV_ALGORITHM_TEXT = "Solve problem with a chosen optimization algorithm"
+BUTTON_HOMEPAGE_COMPARE_TEXT = "Compare algorithms performances"
+
+SELECT_ONE_ALGORITHM_TEXT = "Alright! Select one of the algorithms below."
+
+BUTTON_RUN_AGAIN = "Run again with different parameters"
+BUTTON_BACK_MAIN_PAGE = "Back to Main Page"
+BUTTON_PLOT_HISTORY = "Plot solution history"
+
+HC = "Hill Climbing"
+SA = "Simulated Annealing"
+TS = "Tabu Search"
+GA = "Genetic Algorithm"
+
+DATASET = "Dataset:"
+CHOOSE_DATASET = "Choose dataset"
+FILES = ["a_example.in",
+         "a_example_2.in",
+         "a_example_3.in",
+         "a_example_4.in",
+         "b_read_on.in",
+         "c_incunabula.in",
+         "d_tough_choices.in",
+         "e_so_many_books.in",
+         "f_libraries_of_the_world.in"]
+INITIAL_SOL_MODE = "Initial solution generation mode"
+INITAL_MODES = ["Random", "Greedy"]
+CHOOSE_MODE = "Choose mode"
+
+TABU_TENURE = "Tabu tenure:"
+N_NEIGHBOURS = "Number of neighbours:"
+MAX_INTER = "Max. number of iterations:"
+RUN = "Run!"
+
+TEMP = "Temperature:"
+COOL_SCHED = "Cooling schedule:"
+
+POP_SIZE = "Population size (min. 10):"
+N_GENERATIONS = "Number of generations:"
+MUT_MODE = "Mutation mode:"
+MUTATION_MODES = ["Internal Swap",
+                  "External Swap",
+                  "Deletion",
+                  "Random"]
+CROSS_MODE = "Crossover mode"
+CROSSOVER_MODES = ["Mid point", "Random point"]
+
+RUNNING = "Running..."
