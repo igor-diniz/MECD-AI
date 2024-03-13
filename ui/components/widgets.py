b05f89c5ecf83ddc0b1238130da3f9c397fd695d
paulaito <pakemi.ito@gmail.com>
2024-03-13 15:11:23 +0000

    Created buttons

--- ui/components/widgets.py
+++ ui/components/widgets.py
@@ -1,4 +1,4 @@
-from tkinter import Button, OptionMenu, Frame, Scrollbar, Text, VERTICAL
+from tkinter import Button, OptionMenu, Checkbutton, Frame, Scrollbar, Text, VERTICAL
 from ui.configs.constants import *
 from ui.utils import *
 from helpers.utils import plot_solution_history
@@ -27,6 +27,43 @@ class BackHomePageButton(Button):
                          command=lambda:go_to_page(master, page)
                          )
 
+class RunAlgorithmButton(Button):
+     def __init__(self, master, command, text=RUN):
+        super().__init__(master=master,
+                         text=text,
+                         font=FONT_RUN,
+                         command=command
+                         )
+
+class PlotSolutionHistoryButton(Button):
+     def __init__(self, master, id, algorithm):
+        super().__init__(master=master,
+                         text=BUTTON_PLOT_HISTORY,
+                         font=FONT_SUBTITLE,
+                         command=lambda: plot_solution_history(
+                              id=id,
+                              algorithm=''.join(word[0] for word in algorithm.split()),
+                              save=False
+                          ))
+
+class BackRunAgainButtonFrame():
+    def __init__(self, master, algorithm_page, home_page):
+        self.button_frame = Frame(master)
+        self.button_frame.pack(side="bottom", anchor="c")  # Align the frame to the top-right corner
+
+        # Back to Algorithm button
+        RunAgainButton(self.button_frame, algorithm_page).pack(padx=30, pady=0)
+
+        # Back to Main Page button
+        GoToPageButton(self.button_frame, BUTTON_BACK_MAIN_PAGE, home_page).pack(padx=20, pady=20)
+
+class AlgorithmCheckbutton(Checkbutton):
+     def __init__(self, master, text, variable):
+            super().__init__(master,
+                             text=text,
+                             variable=variable,
+                             font=FONT_TEXT)
+
 class ChooseDatasetMenu(OptionMenu):
     def __init__(self, master, var):
             super().__init__(master,
@@ -50,38 +87,6 @@ class ChooseCrossoverModeMenu(OptionMenu):
             super().__init__(master,
                             var,
                             *CROSSOVER_MODES)
-
-class RunAlgorithmButton(Button):
-     def __init__(self, master, command):
-        super().__init__(master=master,
-                         text=RUN,
-                         font=FONT_RUN,
-                         command=command
-                         )
-        
-
-class PlotSolutionHistoryButton(Button):
-     def __init__(self, master, id, algorithm):
-        super().__init__(master=master,
-                         text=BUTTON_PLOT_HISTORY,
-                         font=FONT_SUBTITLE,
-                         command=lambda: plot_solution_history(
-                              id=id,
-                              algorithm=''.join(word[0] for word in algorithm.split()),
-                              save=False
-                          ))
-
-
-class BackRunAgainButtonFrame():
-    def __init__(self, master, algorithm_page, home_page):
-        self.button_frame = Frame(master)
-        self.button_frame.pack(side="bottom", anchor="c")  # Align the frame to the top-right corner
-
-        # Back to Algorithm button
-        RunAgainButton(self.button_frame, algorithm_page).pack(padx=30, pady=0)
-
-        # Back to Main Page button
-        GoToPageButton(self.button_frame, BUTTON_BACK_MAIN_PAGE, home_page).pack(padx=20, pady=20)
 
 class ScrollBarTextOutput():
     def __init__(self, master):
