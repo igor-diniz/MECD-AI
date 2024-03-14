d336f65b9ae15971cd2922cff110b7848f6b7d11
paulaito <pakemi.ito@gmail.com>
2024-03-14 12:28:30 +0000

    Included configs for MVC

---
+++ mvc/configs.py
@@ -0,0 +1,9 @@
+FILES = ["a_example.in",
+         "a_example_2.in",
+         "a_example_3.in",
+         "a_example_4.in",
+         "b_read_on.in",
+         "c_incunabula.in",
+         "d_tough_choices.in",
+         "e_so_many_books.in",
+         "f_libraries_of_the_world.in"]
