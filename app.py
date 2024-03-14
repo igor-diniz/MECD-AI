ea4b92631f5a2dc98959e012ad5045c04358f5d3
paulaito <pakemi.ito@gmail.com>
2024-03-14 12:28:12 +0000

    Changed app initializer

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from ui.components.front import App
+from mvc.models.main import Model
+from mvc.views.main import View
+from mvc.controllers.main import Controller
+
+
+def main():
+    model = Model()
+    view = View()
+    controller = Controller(model, view)
+    controller.start()
 
 if __name__ == "__main__":
-    root = App()
-    root.homepage()
-    root.mainloop()
+    main()
